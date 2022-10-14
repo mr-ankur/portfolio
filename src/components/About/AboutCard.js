b2202a66f9e9784385559458dfa3530fef7249f3
Ankur Gupta <ankur11.kmr@gmail.com>
2022-10-14 18:10:25 +0530

    resume and data update

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,7 +10,7 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Ankur Gupta </span>
             from <span className="purple"> Uttar Pradesh, India.</span>
-            <br />I am Senior Software Developer at Webonise Lab Pvt Ltd.
+            <br />I am Senior Software Developer at Ticketmaster India.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
