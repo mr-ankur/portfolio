72b5fc7d9445bf770724a8312e59c84bb33b6ec3
Ankur Gupta <ankur11.kmr@gmail.com>
2022-10-30 14:34:40 +0530

    react-github-calendar npm removed

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -1,5 +1,5 @@
 import React from "react";
-import GitHubCalendar from "react-github-calendar";
+// import GitHubCalendar from "react-github-calendar";
 import { Row } from "react-bootstrap";
 
 function Github() {
@@ -17,13 +17,13 @@ function Github() {
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
         Days I <strong className="purple">Code</strong>
       </h1>
-      <GitHubCalendar
+      {/* <GitHubCalendar
         username="mr-ankur"
         blockSize={15}
         blockMargin={5}
         theme={colourTheme}
         fontSize={16}
-      />
+      /> */}
     </Row>
   );
 }
